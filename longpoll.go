c4a534a265ed52d4bff8cc6ab6fd5433e8d1a92f
chazari <chalimovrz@gmail.com>
2023-05-06 16:55:11 +0500

    *_*

--- longpoll.go
+++ longpoll.go
@@ -192,7 +192,8 @@ func (client *VKClient) ListenLongPollServerWithCancel(cancelCtx context.Context
 
 		body, err := client.longpollRequest(server)
 		if err != nil {
-			if !strings.Contains(err.Error(), "context deadline exceeded") {
+			if !strings.Contains(err.Error(), "context deadline exceeded") &&
+				!strings.Contains(err.Error(), "i/o timeout") {
 				log.Printf("longpoll request failed: %s", err)
 			}
 			time.Sleep(time.Second * 5)
